b1b316924200aebca1a14aa4bf14685f24f81043
T. B. <berchet.thomas@gmail.com>
2025-05-21 09:36:18 +0200

    Fix: Missing Tensorflow import in main.py

--- api/main.py
+++ api/main.py
@@ -4,6 +4,7 @@ import json
 import tempfile
 import shutil
 import uuid
+import transformers
 from contextlib import asynccontextmanager
 from fastapi import FastAPI, HTTPException
 from fastapi.middleware.cors import CORSMiddleware
