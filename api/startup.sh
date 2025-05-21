0a8272bee1c1554c92f6fad91324cdee31bed3ae
T. B. <berchet.thomas@gmail.com>
2025-05-21 03:11:31 +0200

    Fix: startup path -2

--- api/startup.sh
+++ api/startup.sh
@@ -5,10 +5,10 @@ cd `dirname $0`
 export PORT=8000
 
 # Activer le mode simulation si nécessaire
-export SIMULATION_MODE=false
+export SIMULATION_MODE=true
 
 # Installer les dépendances
-pip install -r api/requirements.txt
+pip install -r requirements.txt
 
 # Démarrer l'application
 gunicorn main:app --bind=0.0.0.0:$PORT --timeout 600 --workers 2 --worker-class uvicorn.workers.UvicornWorker
